64bc390457fdf2bb8fcfc3f9bef6382c37d94cbe
NotaLink <32184454+NotaLink@users.noreply.github.com>
2017-11-28 07:31:14 -0500

    Delete test.h

--- test.h
+++
@@ -1,7 +0,0 @@
-/* Allegro datafile object indexes, produced by grabber v4.2.0, MinGW32 */
-/* Datafile: c:\Users\apari\Desktop\final12\ */
-/* Date: Sat Nov 25 10:21:12 2017 */
-/* Do not hand edit! */
-
-#define gh                               0        /* BMP  */
-
